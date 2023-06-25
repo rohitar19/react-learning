2e7742ca48b76ff182103e4734ddfe03679d1ed8
rohitar19 <rohit960211@gmail.com>
2023-06-25 23:35:46 +0530

    2nd day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, {Component, createElement} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HI , i am rohit app</h1>
+      <p>This is really working</p>
+      <Person name="ROhit" age ="23"> my hobbies is playing cricket</Person>
+      <Person name = "shine" age= "22"></Person>
+     
+      
     </div>
   );
+
+  // return React.createElement('div' , {className: 'App'} , createElement('h1' , null , 'does this work?'));
+}
 }
 
 export default App;
+ 
